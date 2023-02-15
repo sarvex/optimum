649152eda226a2d738d2ce404a7872c4f285b20f
Ella Charlaix <ella@huggingface.co>
2023-02-15 15:03:08 +0000

    add stable diffusion loading

--- optimum/onnxruntime/modeling_diffusion.py
+++ optimum/onnxruntime/modeling_diffusion.py
@@ -1,22 +1,17 @@
 import importlib
-import inspect
 import logging
 import os
 import shutil
-from abc import ABC
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Any, Callable, Dict, List, Optional, Union
 
 import numpy as np
 import torch
-from diffusers import ConfigMixin, DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler, StableDiffusionPipeline
-from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
+from diffusers import DDIMScheduler, LMSDiscreteScheduler, PNDMScheduler, StableDiffusionPipeline
 from diffusers.schedulers.scheduling_utils import SCHEDULER_CONFIG_NAME
 from diffusers.utils import CONFIG_NAME
 from huggingface_hub import snapshot_download
-from PIL import Image
-from tqdm.auto import tqdm
 from transformers import CLIPFeatureExtractor, CLIPTokenizer
 
 import onnxruntime as ort
@@ -37,11 +32,12 @@ from .utils import (
     validate_provider_availability,
 )
 
+from ..pipelines.pipeline_stable_diffusion import StableDiffusionPipelineMixin
 
 logger = logging.getLogger(__name__)
 
 
-class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
+class ORTModelForStableDiffusion(ORTModel, StableDiffusionPipelineMixin):
     """ """
 
     auto_model_class = StableDiffusionPipeline
@@ -58,13 +54,10 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         scheduler: Union[DDIMScheduler, PNDMScheduler, LMSDiscreteScheduler],
         feature_extractor: CLIPFeatureExtractor,
         config: Dict[str, Any],
-        safety_checker_session: Optional[ort.InferenceSession] = None,
-        # requires_safety_checker: bool = False,
         use_io_binding: Optional[bool] = None,
         model_save_dir: Optional[Union[str, Path, TemporaryDirectory]] = None,
         **kwargs,
     ):
-        ABC.__init__(self)
 
         self.shared_attributes_init(
             vae_decoder_session,
@@ -78,16 +71,19 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         self.text_encoder_model_path = Path(text_encoder_session._model_path)
         self.unet = ORTModelUnet(unet_session, self)
         self.unet_model_path = Path(unet_session._model_path)
-        self._config = config
         self.tokenizer = tokenizer
         self.scheduler = scheduler
         self.feature_extractor = feature_extractor
         self.safety_checker = None
+        sub_models = {"text_encoder": self.text_encoder, "unet": self.unet, "vae_decoder": self.vae_decoder}
+        for name in sub_models.keys():
+            config[name] = ("optimum", sub_models[name].__class__.__name__)
+        config.pop("vae", None)
         self._internal_dict = config
 
     @staticmethod
     def load_model(
-        vae_decoder_path: Optional[Union[str, Path]],
+        vae_decoder_path: Union[str, Path],
         text_encoder_path: Union[str, Path],
         unet_path: Union[str, Path],
         provider: str = "CPUExecutionProvider",
@@ -115,10 +111,10 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
             self.vae_decoder_model_path: save_directory / "vae_decoder" / vae_decoder_file_name,
         }
 
-        # Add external data paths in case of large models
         # TODO: Modify _get_external_data_paths to give dictionnary
         src_paths = list(src_to_dst_path.keys())
         dst_paths = list(src_to_dst_path.values())
+        # Add external data paths in case of large models
         src_paths, dst_paths = _get_external_data_paths(src_paths, dst_paths)
 
         for src_path, dst_path in zip(src_paths, dst_paths):
@@ -138,12 +134,10 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         config: Dict[str, Any],
         use_auth_token: Optional[Union[bool, str]] = None,
         revision: Optional[str] = None,
-        force_download: bool = False,
         cache_dir: Optional[str] = None,
         text_encoder_file_name: str = ONNX_WEIGHTS_NAME,
         unet_file_name: str = ONNX_WEIGHTS_NAME,
         vae_decoder_file_name: str = ONNX_WEIGHTS_NAME,
-        subfolder: str = "",
         local_files_only: bool = False,
         provider: str = "CPUExecutionProvider",
         session_options: Optional[ort.SessionOptions] = None,
@@ -153,8 +147,8 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         **kwargs,
     ):
         model_id = str(model_id)
-        init_dict, unused_kwargs, _ = cls.extract_init_dict(config)
-        sub_model_to_load = set(init_dict.keys()).intersection({"feature_extractor", "tokenizer", "scheduler"})
+        sub_models_to_load, _, _ = cls.extract_init_dict(config)
+        sub_models_names = set(sub_models_to_load.keys()).intersection({"feature_extractor", "tokenizer", "scheduler"})
         sub_models = {}
 
         if not os.path.isdir(model_id):
@@ -169,7 +163,7 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
                     cls.config_name,
                 }
             )
-            # Download all allow_patterns
+            # Downloads all repo's files matching the allowed patterns
             model_id = snapshot_download(
                 model_id,
                 cache_dir=cache_dir,
@@ -181,9 +175,9 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
             )
         new_model_save_dir = Path(model_id)
 
-        for name in sub_model_to_load:
-            library = importlib.import_module(init_dict[name][0])
-            class_obj = getattr(library, init_dict[name][1])
+        for name in sub_models_names:
+            library = importlib.import_module(sub_models_to_load[name][0])
+            class_obj = getattr(library, sub_models_to_load[name][1])
             load_method = getattr(class_obj, "from_pretrained")
             # Check if the module is in a subdirectory
             if (new_model_save_dir / name).is_dir():
@@ -191,7 +185,6 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
             else:
                 sub_models[name] = load_method(new_model_save_dir)
 
-        # Add infer_onnx_filename + _generate_regular_names_for_filename
         text_encoder_path = new_model_save_dir / "text_encoder" / text_encoder_file_name
         unet_path = new_model_save_dir / "unet" / unet_file_name
         vae_decoder_path = new_model_save_dir / "vae_decoder" / vae_decoder_file_name
@@ -256,7 +249,6 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
             force_download=force_download,
             trust_remote_code=trust_remote_code,
         )
-
         output_names = [
             os.path.join("text_encoder", ONNX_WEIGHTS_NAME),
             os.path.join("unet", ONNX_WEIGHTS_NAME),
@@ -266,8 +258,6 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         models_and_onnx_configs = get_stable_diffusion_models_for_export(model)
 
         model.save_config(save_dir_path)
-
-        # maybe_save_preprocessors(model_id, save_dir_path, src_subfolder=subfolder)
         model.tokenizer.save_pretrained(save_dir_path.joinpath("tokenizer"))
         model.scheduler.save_pretrained(save_dir_path.joinpath("scheduler"))
         model.feature_extractor.save_pretrained(save_dir_path.joinpath("feature_extractor"))
@@ -308,254 +298,13 @@ class ORTModelForStableDiffusion(ORTModel, ConfigMixin, ABC):
         self.vae_decoder.session.set_providers([provider], provider_options=[provider_options])
         self.providers = self.text_encoder.session.get_providers()
         return self
+    
+    def __call__(self, *args, **kwargs):
+        return StableDiffusionPipelineMixin.__call__(self, *args, **kwargs)
 
     @classmethod
     def _load_config(cls, config_name_or_path: Union[str, os.PathLike], **kwargs):
         return cls.load_config(config_name_or_path, **kwargs)
-
-    # Copied from https://github.com/huggingface/diffusers/blob/v0.12.1/src/diffusers/pipelines/stable_diffusion/pipeline_onnx_stable_diffusion.py#L115
-    def _encode_prompt(self, prompt, num_images_per_prompt, do_classifier_free_guidance, negative_prompt):
-        r"""
-        Encodes the prompt into text encoder hidden states.
-
-        Args:
-            prompt (`str` or `List[str]`):
-                prompt to be encoded
-            num_images_per_prompt (`int`):
-                number of images that should be generated per prompt
-            do_classifier_free_guidance (`bool`):
-                whether to use classifier free guidance or not
-            negative_prompt (`str` or `List[str]`):
-                The prompt or prompts not to guide the image generation. Ignored when not using guidance (i.e., ignored
-                if `guidance_scale` is less than `1`).
-        """
-        batch_size = len(prompt) if isinstance(prompt, list) else 1
-
-        # get prompt text embeddings
-        text_inputs = self.tokenizer(
-            prompt,
-            padding="max_length",
-            max_length=self.tokenizer.model_max_length,
-            truncation=True,
-            return_tensors="np",
-        )
-        text_input_ids = text_inputs.input_ids
-        untruncated_ids = self.tokenizer(prompt, padding="max_length", return_tensors="np").input_ids
-
-        if not np.array_equal(text_input_ids, untruncated_ids):
-            removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
-            logger.warning(
-                "The following part of your input was truncated because CLIP can only handle sequences up to"
-                f" {self.tokenizer.model_max_length} tokens: {removed_text}"
-            )
-
-        prompt_embeds = self.text_encoder(input_ids=text_input_ids.astype(np.int32))[0]
-        prompt_embeds = np.repeat(prompt_embeds, num_images_per_prompt, axis=0)
-
-        # get unconditional embeddings for classifier free guidance
-        if do_classifier_free_guidance:
-            uncond_tokens: List[str]
-            if negative_prompt is None:
-                uncond_tokens = [""] * batch_size
-            elif type(prompt) is not type(negative_prompt):
-                raise TypeError(
-                    f"`negative_prompt` should be the same type to `prompt`, but got {type(negative_prompt)} !="
-                    f" {type(prompt)}."
-                )
-            elif isinstance(negative_prompt, str):
-                uncond_tokens = [negative_prompt] * batch_size
-            elif batch_size != len(negative_prompt):
-                raise ValueError(
-                    f"`negative_prompt`: {negative_prompt} has batch size {len(negative_prompt)}, but `prompt`:"
-                    f" {prompt} has batch size {batch_size}. Please make sure that passed `negative_prompt` matches"
-                    " the batch size of `prompt`."
-                )
-            else:
-                uncond_tokens = negative_prompt
-
-            max_length = text_input_ids.shape[-1]
-            uncond_input = self.tokenizer(
-                uncond_tokens,
-                padding="max_length",
-                max_length=max_length,
-                truncation=True,
-                return_tensors="np",
-            )
-            negative_prompt_embeds = self.text_encoder(input_ids=uncond_input.input_ids.astype(np.int32))[0]
-            negative_prompt_embeds = np.repeat(negative_prompt_embeds, num_images_per_prompt, axis=0)
-
-            # For classifier free guidance, we need to do two forward passes.
-            # Here we concatenate the unconditional and text embeddings into a single batch
-            # to avoid doing two forward passes
-            prompt_embeds = np.concatenate([negative_prompt_embeds, prompt_embeds])
-
-        return prompt_embeds
-
-    # Adapted from https://github.com/huggingface/diffusers/blob/v0.12.1/src/diffusers/pipelines/stable_diffusion/pipeline_onnx_stable_diffusion.py#L192
-    def __call__(
-        self,
-        prompt: Union[str, List[str]],
-        height: Optional[int] = 512,
-        width: Optional[int] = 512,
-        num_inference_steps: Optional[int] = 50,
-        guidance_scale: Optional[float] = 7.5,
-        negative_prompt: Optional[Union[str, List[str]]] = None,
-        num_images_per_prompt: Optional[int] = 1,
-        eta: Optional[float] = 0.0,
-        generator: Optional[np.random.RandomState] = None,
-        latents: Optional[np.ndarray] = None,
-        output_type: Optional[str] = "pil",
-        return_dict: bool = True,
-        callback: Optional[Callable[[int, int, np.ndarray], None]] = None,
-        callback_steps: Optional[int] = 1,
-    ):
-        if isinstance(prompt, str):
-            batch_size = 1
-        elif isinstance(prompt, list):
-            batch_size = len(prompt)
-        else:
-            raise ValueError(f"`prompt` has to be of type `str` or `list` but is {type(prompt)}")
-
-        if height % 8 != 0 or width % 8 != 0:
-            raise ValueError(f"`height` and `width` have to be divisible by 8 but are {height} and {width}.")
-
-        if (callback_steps is None) or (
-            callback_steps is not None and (not isinstance(callback_steps, int) or callback_steps <= 0)
-        ):
-            raise ValueError(
-                f"`callback_steps` has to be a positive integer but is {callback_steps} of type"
-                f" {type(callback_steps)}."
-            )
-
-        if generator is None:
-            generator = np.random
-
-        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
-        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
-        # corresponds to doing no classifier free guidance.
-        do_classifier_free_guidance = guidance_scale > 1.0
-
-        prompt_embeds = self._encode_prompt(
-            prompt, num_images_per_prompt, do_classifier_free_guidance, negative_prompt
-        )
-
-        # get the initial random noise unless the user supplied it
-        latents_dtype = prompt_embeds.dtype
-        latents_shape = (batch_size * num_images_per_prompt, 4, height // 8, width // 8)
-        if latents is None:
-            latents = generator.randn(*latents_shape).astype(latents_dtype)
-        elif latents.shape != latents_shape:
-            raise ValueError(f"Unexpected latents shape, got {latents.shape}, expected {latents_shape}")
-
-        # set timesteps
-        self.scheduler.set_timesteps(num_inference_steps)
-
-        latents = latents * np.float64(self.scheduler.init_noise_sigma)
-
-        # prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
-        # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
-        # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
-        # and should be between [0, 1]
-        accepts_eta = "eta" in set(inspect.signature(self.scheduler.step).parameters.keys())
-        extra_step_kwargs = {}
-        if accepts_eta:
-            extra_step_kwargs["eta"] = eta
-
-        timestep_dtype = self.unet.input_dtype.get("timestep", np.float32)
-
-        for i, t in enumerate(self.progress_bar(self.scheduler.timesteps)):
-            # expand the latents if we are doing classifier free guidance
-            latent_model_input = np.concatenate([latents] * 2) if do_classifier_free_guidance else latents
-            latent_model_input = self.scheduler.scale_model_input(torch.from_numpy(latent_model_input), t)
-            latent_model_input = latent_model_input.cpu().numpy()
-
-            # predict the noise residual
-            timestep = np.array([t], dtype=timestep_dtype)
-            noise_pred = self.unet(sample=latent_model_input, timestep=timestep, encoder_hidden_states=prompt_embeds)
-            noise_pred = noise_pred[0]
-
-            # perform guidance
-            if do_classifier_free_guidance:
-                noise_pred_uncond, noise_pred_text = np.split(noise_pred, 2)
-                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-
-            # compute the previous noisy sample x_t -> x_t-1
-            scheduler_output = self.scheduler.step(
-                torch.from_numpy(noise_pred), t, torch.from_numpy(latents), **extra_step_kwargs
-            )
-            latents = scheduler_output.prev_sample.numpy()
-
-            # call the callback, if provided
-            if callback is not None and i % callback_steps == 0:
-                callback(i, t, latents)
-
-        latents = 1 / 0.18215 * latents
-        # image = self.vae_decoder(latent_sample=latents)[0]
-        # it seems likes there is a strange result for using half-precision vae decoder if batchsize>1
-        image = np.concatenate(
-            [self.vae_decoder(latent_sample=latents[i : i + 1])[0] for i in range(latents.shape[0])]
-        )
-
-        image = np.clip(image / 2 + 0.5, 0, 1)
-        image = image.transpose((0, 2, 3, 1))
-
-        if self.safety_checker is not None:
-            safety_checker_input = self.feature_extractor(
-                self.numpy_to_pil(image), return_tensors="np"
-            ).pixel_values.astype(image.dtype)
-
-            image, has_nsfw_concepts = self.safety_checker(clip_input=safety_checker_input, images=image)
-
-            # There will throw an error if use safety_checker batchsize>1
-            images, has_nsfw_concept = [], []
-            for i in range(image.shape[0]):
-                image_i, has_nsfw_concept_i = self.safety_checker(
-                    clip_input=safety_checker_input[i : i + 1], images=image[i : i + 1]
-                )
-                images.append(image_i)
-                has_nsfw_concept.append(has_nsfw_concept_i[0])
-            image = np.concatenate(images)
-        else:
-            has_nsfw_concept = None
-
-        if output_type == "pil":
-            image = self.numpy_to_pil(image)
-
-        if not return_dict:
-            return (image, has_nsfw_concept)
-
-        return StableDiffusionPipelineOutput(images=image, nsfw_content_detected=has_nsfw_concept)
-
-    def progress_bar(self, iterable=None, total=None):
-        if not hasattr(self, "_progress_bar_config"):
-            self._progress_bar_config = {}
-        elif not isinstance(self._progress_bar_config, dict):
-            raise ValueError(
-                f"`self._progress_bar_config` should be of type `dict`, but is {type(self._progress_bar_config)}."
-            )
-
-        if iterable is not None:
-            return tqdm(iterable, **self._progress_bar_config)
-        elif total is not None:
-            return tqdm(total=total, **self._progress_bar_config)
-        else:
-            raise ValueError("Either `total` or `iterable` has to be defined.")
-
-    @staticmethod
-    def numpy_to_pil(images):
-        """
-        Convert a numpy image or a batch of images to a PIL image.
-        """
-        if images.ndim == 3:
-            images = images[None, ...]
-        images = (images * 255).round().astype("uint8")
-        if images.shape[-1] == 1:
-            # special case for grayscale (single channel) images
-            pil_images = [Image.fromarray(image.squeeze(), mode="L") for image in images]
-        else:
-            pil_images = [Image.fromarray(image) for image in images]
-
-        return pil_images
 
 
 class ORTModelTextEncoder(ORTModelPart):
--- optimum/pipelines/pipeline_stable_diffusion.py
+++ optimum/pipelines/pipeline_stable_diffusion.py
@@ -13,15 +13,98 @@
 #  limitations under the License.
 
 import inspect
-from typing import Callable, List, Optional, Union
-
+import logging
+from typing import Any, Callable, Dict, List, Optional, Union
 import numpy as np
 import torch
-from diffusers import OnnxStableDiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
+from PIL import Image
+from .pipeline_utils import DiffusionPipelineMixin
+
+logger = logging.getLogger(__name__)
 
 
-class StableDiffusionPipeline(OnnxStableDiffusionPipeline):
+
+class StableDiffusionPipelineMixin(DiffusionPipelineMixin):
+
+    # Copied from https://github.com/huggingface/diffusers/blob/v0.12.1/src/diffusers/pipelines/stable_diffusion/pipeline_onnx_stable_diffusion.py#L115
+    def _encode_prompt(self, prompt:Union[str, List[str]], num_images_per_prompt: int, do_classifier_free_guidance: bool, negative_prompt:Union[str, List[str]]):
+        r"""
+        Encodes the prompt into text encoder hidden states.
+
+        Args:
+            prompt (`str` or `List[str]`):
+                prompt to be encoded
+            num_images_per_prompt (`int`):
+                number of images that should be generated per prompt
+            do_classifier_free_guidance (`bool`):
+                whether to use classifier free guidance or not
+            negative_prompt (`str` or `List[str]`):
+                The prompt or prompts not to guide the image generation. Ignored when not using guidance (i.e., ignored
+                if `guidance_scale` is less than `1`).
+        """
+        batch_size = len(prompt) if isinstance(prompt, list) else 1
+
+        # get prompt text embeddings
+        text_inputs = self.tokenizer(
+            prompt,
+            padding="max_length",
+            max_length=self.tokenizer.model_max_length,
+            truncation=True,
+            return_tensors="np",
+        )
+        text_input_ids = text_inputs.input_ids
+        untruncated_ids = self.tokenizer(prompt, padding="max_length", return_tensors="np").input_ids
+
+        if not np.array_equal(text_input_ids, untruncated_ids):
+            removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
+            logger.warning(
+                "The following part of your input was truncated because CLIP can only handle sequences up to"
+                f" {self.tokenizer.model_max_length} tokens: {removed_text}"
+            )
+
+        prompt_embeds = self.text_encoder(input_ids=text_input_ids.astype(np.int32))[0]
+        prompt_embeds = np.repeat(prompt_embeds, num_images_per_prompt, axis=0)
+
+        # get unconditional embeddings for classifier free guidance
+        if do_classifier_free_guidance:
+            uncond_tokens: List[str]
+            if negative_prompt is None:
+                uncond_tokens = [""] * batch_size
+            elif type(prompt) is not type(negative_prompt):
+                raise TypeError(
+                    f"`negative_prompt` should be the same type to `prompt`, but got {type(negative_prompt)} !="
+                    f" {type(prompt)}."
+                )
+            elif isinstance(negative_prompt, str):
+                uncond_tokens = [negative_prompt] * batch_size
+            elif batch_size != len(negative_prompt):
+                raise ValueError(
+                    f"`negative_prompt`: {negative_prompt} has batch size {len(negative_prompt)}, but `prompt`:"
+                    f" {prompt} has batch size {batch_size}. Please make sure that passed `negative_prompt` matches"
+                    " the batch size of `prompt`."
+                )
+            else:
+                uncond_tokens = negative_prompt
+
+            max_length = text_input_ids.shape[-1]
+            uncond_input = self.tokenizer(
+                uncond_tokens,
+                padding="max_length",
+                max_length=max_length,
+                truncation=True,
+                return_tensors="np",
+            )
+            negative_prompt_embeds = self.text_encoder(input_ids=uncond_input.input_ids.astype(np.int32))[0]
+            negative_prompt_embeds = np.repeat(negative_prompt_embeds, num_images_per_prompt, axis=0)
+
+            # For classifier free guidance, we need to do two forward passes.
+            # Here we concatenate the unconditional and text embeddings into a single batch
+            # to avoid doing two forward passes
+            prompt_embeds = np.concatenate([negative_prompt_embeds, prompt_embeds])
+
+        return prompt_embeds
+
     # Adapted from https://github.com/huggingface/diffusers/blob/v0.12.1/src/diffusers/pipelines/stable_diffusion/pipeline_onnx_stable_diffusion.py#L192
     def __call__(
         self,
@@ -65,6 +148,7 @@ class StableDiffusionPipeline(OnnxStableDiffusionPipeline):
         # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
         # corresponds to doing no classifier free guidance.
         do_classifier_free_guidance = guidance_scale > 1.0
+
         prompt_embeds = self._encode_prompt(
             prompt, num_images_per_prompt, do_classifier_free_guidance, negative_prompt
         )
@@ -134,10 +218,7 @@ class StableDiffusionPipeline(OnnxStableDiffusionPipeline):
                 self.numpy_to_pil(image), return_tensors="np"
             ).pixel_values.astype(image.dtype)
 
-            # TODO : Modify the image if has_nsfw_concepts
-            image, has_nsfw_concepts = self.safety_checker(clip_input=safety_checker_input, images=image)
-
-            # There will throw an error if use safety_checker batchsize>1
+            # safety_checker does not support batched inputs yet
             images, has_nsfw_concept = [], []
             for i in range(image.shape[0]):
                 image_i, has_nsfw_concept_i = self.safety_checker(
@@ -156,3 +237,4 @@ class StableDiffusionPipeline(OnnxStableDiffusionPipeline):
             return (image, has_nsfw_concept)
 
         return StableDiffusionPipelineOutput(images=image, nsfw_content_detected=has_nsfw_concept)
+    
